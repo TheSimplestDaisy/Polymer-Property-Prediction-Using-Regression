3b40bbcde1cdba7467fea36e714b4418b0e919d6
zzulkoff <59239137+TheSimplestDaisy@users.noreply.github.com>
2025-08-01 05:29:16 +0800

    Update infer.py

--- infer.py
+++ infer.py
@@ -1,32 +1,33 @@
-
 import torch
-import torch.nn as nn
-import pandas as pd
+import pickle
 from model import TransPolymer
 from PolymerSmilesTokenization import PolymerSmilesTokenizer
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-TARGETS = ['Eea', 'Egb', 'Egc', 'Ei', 'EPS', 'Nc']
-MAX_LENGTH = 175
 
-# Load tokenizer and model
-def load_model_and_tokenizer():
-    tokenizer = PolymerSmilesTokenizer.from_pretrained("ckpt/poc_fast_model.pt")
-    model = TransPolymer()
-    model.regression_head = nn.Linear(model.hidden_size, len(TARGETS))
-    model.load_state_dict(torch.load("runs/final_model.pt", map_location=device))
-    model.to(device)
+TARGETS = ['Eea', 'Egb', 'Egc', 'Ei', 'EPS', 'Nc']
+
+def load_model_and_tokenizer(
+    model_path="ckpt/poc_fast_model.pt",
+    tokenizer_path="tokenizer.pt"
+):
+    # Load tokenizer dari fail pickle
+    with open(tokenizer_path, "rb") as f:
+        tokenizer = pickle.load(f)
+
+    # Load model
+    model = torch.load(model_path, map_location=device)
     model.eval()
+
     return model, tokenizer
 
-# Predict properties from SMILES
-def predict_properties(smiles, model, tokenizer):
-    with torch.no_grad():
-        inputs = tokenizer(smiles, padding='max_length', truncation=True, max_length=MAX_LENGTH, return_tensors="pt")
-        input_ids = inputs["input_ids"].to(device)
-        attention_mask = inputs["attention_mask"].to(device)
+def predict_properties(model, tokenizer, smiles):
+    tokens = tokenizer(smiles)
+    input_ids = torch.tensor([tokens["input_ids"]]).to(device)
+    attention_mask = torch.tensor([tokens["attention_mask"]]).to(device)
 
+    with torch.no_grad():
         outputs = model(input_ids=input_ids, attention_mask=attention_mask)
-        preds = model.regression_head(outputs[:, 0])  # CLS token
-        preds = preds.cpu().numpy().flatten()
-        return dict(zip(TARGETS, preds.tolist()))
+        prediction = outputs.cpu().numpy().flatten()
+
+    return dict(zip(TARGETS, prediction.tolist()))
